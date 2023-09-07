cfb66af2c04be0219b23c1cf65e1032d578c562e
mailinh0806 <vumailinh29122002@gmail.com>
2023-09-07 18:02:16 +0700

    bai5

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,12 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+
 int a,b;
 System.Console.Write("a = ");
 a = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("b = ");
 b = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("{0} - {1} = {2}", a, b, a-b);
-System.Console.WriteLine("{0} * {1} = {2}", a, b, a*b);
-System.Console.WriteLine("{0} / {1} = {2}", a, b, b/a);
-System.Console.WriteLine("{0} % {1} = {2}", a, b, b%a);
+if ( a < b)
+{
+   Console.WriteLine(" a nhỏ hơn b");
+}
+else if (a > b)
+{
+  Console.WriteLine(" a lớn hơn b");  
+}
+else if (a == b)
+{
+ Console.WriteLine(" a bằng b");  
+}   
