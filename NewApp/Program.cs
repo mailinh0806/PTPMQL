ef2a58554d8c118ba049a4de05e54908dba3fd0b
mailinh0806 <vumailinh29122002@gmail.com>
2023-10-02 15:47:35 +0700

    collections

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,21 +1,47 @@
-﻿using NewApp.Models;
+﻿using System.Collections;
+using System.Runtime.Intrinsics.Arm;
+using NewApp.Models;
 public class Program{
     private static void Main(string[] args)
     {
      int n;
-        Console.Write("\n" + "Nhap kich thuoc mang Customer: ");
+        System.Console.Write("Nhap kich thuoc danh sach: ");
         n = Convert.ToInt32(Console.ReadLine());
-        Customer[] cusArr = new Customer[n];
-        for(int i = 0; i < cusArr.Length; i++){
-            Console.WriteLine("Nhap thong tin khach hang thu {0}", i+1);
-            Customer cus = new Customer();
-            cus.NhapThongTin();
-           cusArr[i] = cus;
+        ArrayList stdList = new ArrayList();
+        for(int i = 0; i < n; i++)
+        {
+        System.Console.Write("Nhap thong tin sinh vien thu {0}", i++);
+        Student std = new Student();
+        std.Nhapthongtin();
+        stdList.Add(std);  
+        } 
+        System.Console.Write("Hien thi danh sach ");  
+        foreach(Student std in stdList){
+            std.HienThi();
         }
-        Console.Write("Hien thi mang Employee");
-        foreach(Customer cus in cusArr){
-            cus.HienThi();
-        }   
+        int indexToEdit = 0;
+        if (indexToEdit >= 0 && indexToEdit < stdList.Count)
+        {
+            Console.WriteLine("\n" + "Nhap thong tin moi cho sinh vien thu {0}", indexToEdit + 1);
+            Student newStudentInfo = new Student();
+            newStudentInfo.Nhapthongtin();
+            stdList[indexToEdit] = newStudentInfo;
+        }
+        else
+        {
+            Console.WriteLine("Vi tri khong hop le");
+        }
+
+        int indexToDelete = 1;
+        if (indexToDelete >= 0 && indexToDelete < stdList.Count)
+        {
+            stdList.RemoveAt(indexToDelete);
+            Console.WriteLine("Da xoa sinh vien thu {0}", indexToDelete + 1);
+        }
+        else
+        {
+            Console.WriteLine("Vi tri khong hop le");
+        }
     }
 }
  
